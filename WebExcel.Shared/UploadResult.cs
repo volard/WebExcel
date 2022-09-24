20b46f094033f50a2998e28c531259191d1b4fae
volard <kirillzhilenkov@gmail.com>
2022-09-25 01:33:49 +0800

    :card_file_box: Add file's uploading result class

---
+++ WebExcel.Shared/UploadResult.cs
@@ -0,0 +1,11 @@
+﻿namespace WebExcel.Shared
+{
+    public class UploadResult
+    {
+        public Dictionary<string, List<string>>? Data { get; set; }
+        public bool Uploaded { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
