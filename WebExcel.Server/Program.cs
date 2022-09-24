fd3d570ab71055c820b4dbc965e7533203438825
volard <kirillzhilenkov@gmail.com>
2022-09-24 22:01:40 +0800

    :hammer: Add test API endpoint

--- WebExcel.Server/Program.cs
+++ WebExcel.Server/Program.cs
@@ -3,4 +3,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/test", () => {
+    Dictionary<string, List<String>> info = new Dictionary<string, List<String>>();
+    info["clients"] = new List<string>() { "Tom", "Bob", "Sam" };
+    info["listeners"] = new List<string>() { "asdf", "sdfafsdas", "Sasdfafafsasm" };
+    return info;
+});
+
+
 app.Run();
