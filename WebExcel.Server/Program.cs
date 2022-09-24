580d4099d0c58ccc4c860119e7b807bc5872ded5
volard <kirillzhilenkov@gmail.com>
2022-09-24 23:45:44 +0800

    :boom: Setup client - server data exchanging

--- WebExcel.Server/Program.cs
+++ WebExcel.Server/Program.cs
@@ -1,12 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.WithOrigins("https://localhost:7062")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod());
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/test", () => {
-    Dictionary<string, List<String>> info = new Dictionary<string, List<String>>();
-    info["clients"] = new List<string>() { "Tom", "Bob", "Sam" };
-    info["listeners"] = new List<string>() { "asdf", "sdfafsdas", "Sasdfafafsasm" };
+app.MapPost("/test", () =>
+{
+    Dictionary<string, List<String>> info = new()
+    {
+        ["clients"] = new List<string>() { "Tom", "Bob", "Sam" },
+        ["listeners"] = new List<string>() { "asdf", "sdfafsdas", "Sasdfafafsasm" }
+    };
     return info;
 });
 
