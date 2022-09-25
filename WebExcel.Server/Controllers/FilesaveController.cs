23d5e78cb49e7c37cfa559b8798cbe74b2151cdc
volard <kirillzhilenkov@gmail.com>
2022-09-25 17:53:47 +0800

    :boom: Add file saving support

--- WebExcel.Server/Controllers/FilesaveController.cs
+++ WebExcel.Server/Controllers/FilesaveController.cs
@@ -1,10 +1,22 @@
 ﻿//using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
-using WebExcel.Shared;
+using System.Text;
+//using WebExcel.Shared;
 
 namespace WebExcel.Server.Controllers
 {
+    public class UploadResult
+    {
+        public Dictionary<string, List<string>>? Data { get; set; }
+        public bool Uploaded { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public int ErrorCode { get; set; }
+    }
+
+
     [ApiController]
     [Route("api/[controller]")]
     public class FilesaveController : ControllerBase
@@ -19,20 +31,64 @@ namespace WebExcel.Server.Controllers
             this.logger = logger;
         }
 
+        [EnableCors("MyAllowSpecificOrigins")]
         [HttpPost]
-        public async Task<ActionResult<UploadResult>> PostFile(
+        public async Task<IActionResult> PostFile(
             [FromForm] IFormFile file)
+            //)
         {
+
+            //logger.LogInformation(Request.QueryString.ToString());
+            //logger.LogInformation(Request.Headers.ToString());
+            //logger.LogInformation(Request.Method.ToString());
+
+            //foreach (var header in Request.Headers)
+            //{
+            //    logger.LogInformation(header.Key + " : " + header.Value + "\n==================================");
+            //}
+            //logger.LogInformation("Uploaded actual filename" + " : " + file.FileName + "\n==================================");
+
+            //logger.LogInformation("\n==================================");
+            //logger.LogInformation("\n==================================");
+
+            //foreach(var header in Response.Headers)
+            //{
+            //    logger.LogInformation(header.Key + " : " + header.Value + "\n==================================");
+            //}
+
+            //UploadResult res = new()
+            //{
+            //    FileName = "testName",
+            //    Uploaded = true,
+            //    StoredFileName = "StoredName.txt"
+            //};
+
+
             
-            long maxFileSize = 1024 * 65;
+
+
+
+
+
+
+
+  //          env.response.headers["Access-Control-Allow-Methods"] = "POST"
+  //# ...with `Content-type` header in the request...
+  //env.response.headers["Access-Control-Allow-Headers"] = "Content-type"
+
+
+            //return res;
+            
+
+
+            long maxFileSize = 1024 * 1000;
             //var resourcePath = new Uri($"{Request.Scheme}://{Request.Host}/");
 
             var uploadResult = new UploadResult();
-            string trustedFileNameForFileStorage;
+            //string trustedFileNameForFileStorage;
             var untrustedFileName = file.FileName;
             uploadResult.FileName = untrustedFileName;
             var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
-            logger.LogInformation(Request.ToString());
 
             if (file.Length == 0)
             {
@@ -52,30 +108,46 @@ namespace WebExcel.Server.Controllers
                 try
                 {
                     //trustedFileNameForFileStorage = Path.GetRandomFileName() + Path.GetExtension(file.FileName);
-                    trustedFileNameForFileStorage = Path.GetRandomFileName();
-                    var path = Path.Combine(env.ContentRootPath,
-                        env.EnvironmentName, "Uploads",
-                        trustedFileNameForFileStorage);
-                    Console.WriteLine(path);
+                    //trustedFileNameForFileStorage = Path.GetRandomFileName();
+                    //var path = Path.Combine(env.ContentRootPath,
+                    //    env.EnvironmentName, "Uploads",
+                    //    trustedFileNameForFileStorage);
+                    //Console.WriteLine(path);
 
-                    await using FileStream fs = new(path, FileMode.Create);
+                    await using FileStream fs = new(env.ContentRootPath + "Development/Uploads/some.xlsx", FileMode.Create);
                     await file.CopyToAsync(fs);
 
-                    logger.LogInformation("{FileName} saved at {Path}",
-                        trustedFileNameForDisplay, path);
+                    //logger.LogInformation("{FileName} saved at {Path}",
+                    //    trustedFileNameForDisplay, path);
 
-                    uploadResult.Uploaded = true;
-                    uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                    //uploadResult.Uploaded = true;
+                    //uploadResult.StoredFileName = trustedFileNameForFileStorage;
                 }
                 catch (IOException ex)
                 {
                     logger.LogError("{FileName} error on upload (Err: 3): {Message}",
-                        trustedFileNameForDisplay, ex.Message);
+                        "somename.txt", ex.Message);
                     uploadResult.ErrorCode = 3;
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("{FileName} error on upload (Err: 3): {Message}",
+                        "somename.txt", ex.Message);
                 }
             }
 
-            return uploadResult;
+            Response.Headers.AccessControlAllowOrigin = "https://localhost:7062"; // Allow-Origin
+            Response.Headers.AccessControlAllowHeaders = "multipart/form-data";
+            //Response.Headers.AccessControlAllowHeaders = ;
+            Response.Headers.AccessControlAllowMethods = "POST";
+            return Ok("asdf");
+            //uploadResult.Data = new()
+            //{
+            //    ["IM FROM SERVER BROOOO"] = new List<string>() { "Tom", "Bob", "Sam", "biba", "pupa" },
+            //    ["clients"] = new List<string>() { "Tom", "Bob", "Sam" },
+            //    ["listeners"] = new List<string>() { "asdf", "sdfafsdas", "Sasdfafafsasm" }
+            //};
+            //return uploadResult;
         }
     }
 }
--- WebExcel.Server/Program.cs
+++ WebExcel.Server/Program.cs
@@ -1,12 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors();
+
+//builder.Services.AddCors();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7062")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.WithOrigins("https://localhost:7062")
-                              .AllowAnyHeader()
-                              .AllowAnyMethod());
+
+//app.UseCors(builder => builder.WithOrigins("https://localhost:7062")
+//                              .AllowAnyHeader()
+//                              .AllowAnyMethod()
+//                              );
+//app.UseCors(builder => builder.AllowAnyOrigin());
 
 app.MapGet("/", () => "Hello World!");
 
@@ -21,5 +37,9 @@ app.MapPost("/test", () =>
 });
 
 app.UseRouting();
+
+app.UseCors();
+
 app.MapControllers();
+
 app.Run();
